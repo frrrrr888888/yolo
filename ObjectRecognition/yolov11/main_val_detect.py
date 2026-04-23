ee8c6e7fab77c0633f8be061a8ebce117b6e914e
furenrui <furenrui@taote.tech>
2026-04-23 14:54:55 +0800

    修改参数

--- ObjectRecognition/yolov11/main_val_detect.py
+++ ObjectRecognition/yolov11/main_val_detect.py
@@ -2,7 +2,7 @@ from ultralytics.models.yolo.detect import DetectionValidator
 
 if __name__ == '__main__':
     args = dict(
-        model=r"",
-                data=r".\ultralytics\cfg\datasets\coco8.yaml")
+                model=r"F:\frr\try\ObjectRecognition\yolov11\runs\detect\train\weights\last.pt",
+                data=r"./ultralytics/cfg/datasets_weed/weed_RF_c2.yaml")
     validator = DetectionValidator(cfg_path=r".\ultralytics\cfg\default_detect_val.yaml",args=args)
     validator()
