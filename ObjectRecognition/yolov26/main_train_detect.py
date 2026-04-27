064db0ee254f4df8681ff36d7e1042007a13ea72
furenrui <furenrui@taote.tech>
2026-04-27 16:23:38 +0800

    配置

--- ObjectRecognition/yolov26/main_train_detect.py
+++ ObjectRecognition/yolov26/main_train_detect.py
@@ -2,7 +2,7 @@ from ultralytics.models.yolo.detect import DetectionTrainer
 
 if __name__ == '__main__':
     args = dict(model=r"./ultralytics/cfg/models/26/yolo26.yaml",
-                data=r"./ultralytics/cfg/datasets_weed/weed_RF_c2.yaml", epochs=300)
+                data=r"./ultralytics/cfg/datasets_weed/weed_RF_20260421_c2.yaml", epochs=300)
     trainer = DetectionTrainer(cfg=r"./ultralytics/cfg/default.yaml",
                                overrides=args)
     trainer.train()
