3dd4ba06f62500d3b894a5eb814bbc47c0a7aebd
furenrui <furenrui@taote.tech>
2026-04-29 16:21:25 +0800

    参数更改

--- ObjectRecognition/yolov11/main_train_detect.py
+++ ObjectRecognition/yolov11/main_train_detect.py
@@ -1,8 +1,9 @@
 from ultralytics.models.yolo.detect import DetectionTrainer
 
 if __name__ == '__main__':
-    args = dict(model=r"./ultralytics/cfg/models/11/yolo11.yaml",
-                data=r"./ultralytics/cfg/datasets_weed/weed_RF_c2.yaml", epochs=600)
+    args = dict(#model=r"./ultralytics/cfg/models/11/yolo11.yaml",
+                model=r"F:\frr\try\ObjectRecognition\yolov11\runs\detect\train6\weights\best.pt",
+                data=r"./ultralytics/cfg/datasets_weed/weed_RF_c2.yaml", epochs=400)
     trainer = DetectionTrainer(cfg=r"./ultralytics/cfg/default_detect.yaml",
                                overrides=args)
     trainer.train()
