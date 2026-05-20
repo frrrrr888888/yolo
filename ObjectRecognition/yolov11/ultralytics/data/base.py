5312d9ffe2ba339d7b9bdaef31d77cfaef6a2d50
furenrui <furenrui@taote.tech>
2026-05-20 15:18:45 +0800

    完善416*640格式输入（set_rectangle）

--- ObjectRecognition/yolov11/ultralytics/data/base.py
+++ ObjectRecognition/yolov11/ultralytics/data/base.py
@@ -255,17 +255,17 @@ class BaseDataset(Dataset):
             elif isinstance(self.imgsz, list):
                 if rect_mode:
 
-                    rh = self.imgsz[0] / max(h0, w0)
-                    rw = self.imgsz[1] / max(h0, w0)
-                    r = max(rh, rw)
-                    #r = min(rh, rw)
+                    rh = self.imgsz[0] / h0
+                    rw = self.imgsz[1] / w0
+                    #r = max(rh, rw)
+                    r = min(rh, rw)
 
                     if r != 1:  # if sizes are not equal
                         #w, h = (min(math.ceil(w0 * r), self.imgsz[1]), min(math.ceil(h0 * r), self.imgsz[0]))
                         w, h = (math.ceil(w0 * r), math.ceil(h0 * r))
                         im = cv2.resize(im, (w, h), interpolation=cv2.INTER_LINEAR)
                 elif not (h0 == self.imgsz[1] and w0 == self.imgsz[0]):  # resize by stretching image to square imgsz
-                    im = cv2.resize(im, (self.imgsz, self.imgsz), interpolation=cv2.INTER_LINEAR)
+                    im = cv2.resize(im, (self.imgsz[0], self.imgsz[1]), interpolation=cv2.INTER_LINEAR)
             else:
                 # 非法类型 → 主动报错并终止程序
                 raise TypeError(f"imgsz 必须是 int 或 list 类型，当前传入类型：{type(self.imgsz)}")
@@ -395,7 +395,13 @@ class BaseDataset(Dataset):
             elif mini > 1:
                 shapes[i] = [1, 1 / mini]
 
-        self.batch_shapes = np.ceil(np.array(shapes) * self.imgsz / self.stride + self.pad).astype(int) * self.stride
+        if isinstance(self.imgsz, list):
+            size = max(self.imgsz)
+        else:
+            size = self.imgsz
+
+        #self.batch_shapes = np.ceil(np.array(shapes) * self.imgsz / self.stride + self.pad).astype(int) * self.stride
+        self.batch_shapes = np.ceil(np.array(shapes) * size / self.stride + self.pad).astype(int) * self.stride
         self.batch = bi  # batch index of image
 
     def __getitem__(self, index: int) -> dict[str, Any]:
